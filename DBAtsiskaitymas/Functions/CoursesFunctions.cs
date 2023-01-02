ac1f0c77762f9a4ec74fb92029a8073515b159d2
BimBaMas <dagilis@gmail.com>
2023-01-02 16:02:45 +0200

    Created all functions for Courses part and created GUI

---
+++ DBAtsiskaitymas/Functions/CoursesFunctions.cs
@@ -0,0 +1,36 @@
+﻿using DBAtsiskaitymas.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAtsiskaitymas.Functions
+{
+    public static class CoursesFunctions
+    {
+        public static void CreateNewCourseAndAddToDepartment(Context dbContext)
+        {
+            Console.Write("Enter name for new course : ");
+            string name = Console.ReadLine();
+            Course course = new Course(name);
+            Console.Write("Enter department ID's for this course separated by space : ");
+            string departments = Console.ReadLine();
+            List<int> departmentsId = departments.Split(' ').Select(n => Convert.ToInt32(n)).ToList();
+            foreach (int departmentId in departmentsId)
+            {
+                course.Departments.Add(dbContext.Departments.Where(x => x.Id == departmentId).First());
+            }
+            dbContext.Courses.Add(course);
+            dbContext.SaveChanges();
+        }
+        public static void PrintCourses(Context dbContext)
+        {
+            var courses = dbContext.Courses;
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"ID {course.Id} Name {course.Name}");
+            }
+        }
+    }
+}
---
+++ DBAtsiskaitymas/Menu.cs
@@ -0,0 +1,55 @@
+﻿using DBAtsiskaitymas.Functions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAtsiskaitymas
+{
+    public static class Menu
+    {
+        public static void Init(Context dbContext)
+        {
+            string menu = "";
+            while (true)
+            {
+                Console.Clear();
+                DrawMenu();
+                Console.Write("Select menu option : ");
+                menu = Console.ReadLine();
+                DrawSubMenu(menu, dbContext);
+            }
+        }
+        public static void DrawSubMenu(string menu, Context dbContext)
+        {
+            switch (menu)
+            {
+                case "1" :
+                    Console.WriteLine("Awailable departments : ");
+                    DepartmentsFunctions.PrintDepartments(dbContext);
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.WriteLine("Awailable courses : ");
+                    CoursesFunctions.PrintCourses(dbContext);
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.WriteLine("Awailable students : ");
+                    StudentsFunctions.PrintStudents(dbContext);
+                    Console.ReadKey();
+                    break;
+                case "Q":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+        public static void DrawMenu()
+        {
+            Console.WriteLine("[1]Departments [2]Courses [3]Students [Q]Exit");
+        }
+    }
+}
